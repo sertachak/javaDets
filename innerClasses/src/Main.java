974d8df524863104e2a7c333ff00c7c2688d390c
sertachak <sertaconurhakbilen@gmail.com>
2021-08-21 12:43:43 +0300

    testOfInnerClass

--- innerClasses/src/Main.java
+++ innerClasses/src/Main.java
@@ -6,5 +6,14 @@ public class Main {
         GearBox.Gear first = mcLaren.new Gear(1, 12.3); // without outer class object cannot create a inner class object error= is not an enclosing class
         System.out.println(first.drivateSpeed(1000));
         */
+        mcLaren.addGear(1, 5.3);
+        mcLaren.addGear(2, 10.6);
+        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(1000));
     }
 }
