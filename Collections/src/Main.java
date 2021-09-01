2650a56963eea94d69e1c844b99d4a774b2f69b8
sertachak <sertaconurhakbilen@gmail.com>
2021-09-01 20:13:24 +0300

    comparator

--- Collections/src/Main.java
+++ Collections/src/Main.java
@@ -16,11 +16,17 @@ public class Main {
         sortList(seatCopy);
         printList(seatCopy);
 
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.seats);
+        priceSeats.add(theatre.new Seat("B00", 13.00));
+        priceSeats.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+        printList(priceSeats);
+
     }
 
     public static void printList(List<Theatre.Seat> list) {
         for(Theatre.Seat seat : list){
-            System.out.print(" " + seat.getSeatNumber());
+            System.out.print(" " + seat.getSeatNumber() + " " + seat.getPrice());
         }
         System.out.println();
         System.out.println("===================================================");
--- Collections/src/Theatre.java
+++ Collections/src/Theatre.java
@@ -1,9 +1,23 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Theatre {
     private final String name;
     public List<Seat> seats = new ArrayList<>();
+
+    static final Comparator<Seat> PRICE_ORDER = new Comparator<Seat>() {
+        @Override
+        public int compare(Seat seat1, Seat seat2) {
+            if(seat1.getPrice() < seat2.getPrice()) {
+                return -1;
+            } else if(seat1.getPrice() > seat2.getPrice()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    };
 
     public Theatre(String name, int numRows, int seatsPerRow) {
         this.name = name;
@@ -11,7 +25,13 @@ public class Theatre {
         int lastRow = 'A' + (numRows-1);
         for(char row = 'A'; row<= lastRow; row++) {
             for(int seatNum = 1; seatNum<=seatsPerRow; seatNum++) {
-                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                double price = 12.00;
+                if((row < 'D') && (seatNum >= 4 || seatNum <= 9)){
+                    price = 14.00;
+                } else if( (row > 'F') || (seatNum < 4 || seatNum > 9 )) {
+                    price = 7.00;
+                }
+                Seat seat = new Seat(row + String.format("%02d", seatNum), price);
                 seats.add(seat);
             }
         }
@@ -40,9 +60,15 @@ public class Theatre {
     class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
+        private final double price;
 
-        public Seat(String seatNumber) {
+        public Seat(String seatNumber, double price) {
             this.seatNumber = seatNumber;
+            this.price = price;
+        }
+
+        public double getPrice() {
+            return price;
         }
 
         public boolean reserve() {
