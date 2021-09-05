21f88b9542d2d637c2f79c0826573c22b50121a7
sertachak <sertaconurhakbilen@gmail.com>
2021-09-05 12:58:22 +0300

    immutable class

--- Adventure/src/MainAdventure.java
+++ Adventure/src/MainAdventure.java
@@ -8,36 +8,48 @@ public class MainAdventure {
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
+        Map<String, Integer> tempExits = new HashMap<>();
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExits));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("N", 5);
-//        locations.get(1).addExit("Q", 0);
 
-        locations.get(2).addExit("N", 5);
-//        locations.get(2).addExit("Q", 0);
+        tempExits = new HashMap<>();
+        tempExits.put("W", 2);
+        tempExits.put("E", 3);
+        tempExits.put("S", 4);
+        tempExits.put("N", 5);
+//        tempExits.put("Q", 0);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExits));
 
-        locations.get(3).addExit("W", 1);
-//        locations.get(3).addExit("Q", 0);
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-//        locations.get(4).addExit("Q", 0);
+        tempExits = new HashMap<>();
+        tempExits.put("N", 5);
+//        tempExits.put("Q", 0);
+        locations.put(2, new Location(2, "You are at the top of a hill", tempExits));
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
-//        locations.get(5).addExit("Q", 0);
+
+        tempExits = new HashMap<>();
+        tempExits.put("W", 1);
+//        tempExits.put("Q", 0);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExits));
+
+        tempExits = new HashMap<>();
+        tempExits.put("N", 1);
+        tempExits.put("W", 2);
+//        tempExits.put("Q", 0);
+        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExits));
+
+
+        tempExits = new HashMap<>();
+        tempExits.put("S", 1);
+        tempExits.put("W", 2);
+//        tempExits.put("Q", 0);
+        locations.put(5, new Location(5, "You are in the forest", tempExits));
+
 
         int loc = 1;
         while(true) {
             int prevLoc = loc;
+            tempExits.remove("S");
             System.out.println(locations.get(loc).getDescription());
             if(loc == 0) {
                 break;
