1efac937f21b8ff1add710d6afa0aa75e1cb0646
sertachak <sertaconurhakbilen@gmail.com>
2021-09-15 17:18:35 +0300

    stockItem -- sortedCollections

---
+++ sortedCollections/src/StockItem.java
@@ -0,0 +1,68 @@
+public class StockItem implements Comparable<StockItem>{
+    private final String name;
+    private double price;
+    private int quantityStock;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantityStock() {
+        return quantityStock;
+    }
+
+    public void setPrice(double price) {
+        if(price > 0) {
+            this.price = price;
+        }
+    }
+
+    public void adjustStock(int quantity) {
+        int newQuantity = this.quantityStock + quantity;
+        if(newQuantity >= 0) {
+            this.quantityStock = newQuantity;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+
+        if((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+
+        String objName = ((StockItem)obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 23;
+    }
+
+    @Override
+    public int compareTo(StockItem o ) {
+        System.out.println("entering StockItem.compareTo");
+        if(this == o){
+            return 0;
+        }
+        if( o != null){
+           return this.name.compareTo(o.getName());
+        }
+
+        throw new NullPointerException();
+    }
+}
