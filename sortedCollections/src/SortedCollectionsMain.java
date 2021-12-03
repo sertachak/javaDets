f697da53e4d7faca563989b9cd18b00e2be66b41
sertachak <sertaconurhakbilen@gmail.com>
2021-12-03 20:13:51 +0300

    Java Request

--- sortedCollections/src/SortedCollectionsMain.java
+++ sortedCollections/src/SortedCollectionsMain.java
@@ -1,7 +1,29 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class SortedCollectionsMain {
     private static StockList stockList = new StockList();
 
-    public static void main(String[] args) {
+
+    public static void executePost(String targetURL, String urlParameters) throws IOException {
+        URL yahoo = new URL("https://jsonplaceholder.typicode.com/posts");
+        URLConnection yc = yahoo.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        yc.getInputStream()));
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+       executePost("https://jsonplaceholder.typicode.com/posts", null);
+
+
         StockItem temp = new StockItem("bread", 0.86, 100);
         stockList.addStock(temp);
 
