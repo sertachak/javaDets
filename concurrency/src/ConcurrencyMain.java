c3bfb7c6a5069f214cc4ea3bc02087e20daa0745
sertachak <sertaconurhakbilen@gmail.com>
2022-01-18 18:37:13 +0300

    runnable and anonymous override

--- concurrency/src/ConcurrencyMain.java
+++ concurrency/src/ConcurrencyMain.java
@@ -8,6 +8,14 @@ public class ConcurrencyMain {
 
         new Thread(() -> System.out.println(ThreadColor.ANSI_GREEN + "From anonymous thread")).start();
 
+        Thread myRunnableThread = new Thread(new MyRunnable(){
+            @Override
+            public void run() {
+                System.out.println(ThreadColor.ANSI_RED + "Hello from anonymous class's implementation myRunnable");
+            }
+        });
+        myRunnableThread.start();
+
         System.out.println(ThreadColor.ANSI_PURPLE + "Main thread again");
 
     }
