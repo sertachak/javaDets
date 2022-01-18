b0daf7ec8e8b91a874c50ef2d06430c50804f197
sertachak <sertaconurhakbilen@gmail.com>
2022-01-18 19:05:08 +0300

    interrupts

--- concurrency/src/AnotherThread.java
+++ concurrency/src/AnotherThread.java
@@ -2,5 +2,14 @@ public class AnotherThread extends Thread{
     @Override
     public void run() {
         System.out.println(ThreadColor.ANSI_BLUE + "Another thread");
+
+        try{
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(ThreadColor.ANSI_BLUE + "Another thread woke me up");
+            return;
+        }
+
+        System.out.println(ThreadColor.ANSI_BLUE + "Three seconds have passed and waked up");
     }
 }
--- concurrency/src/ConcurrencyMain.java
+++ concurrency/src/ConcurrencyMain.java
@@ -11,10 +11,17 @@ public class ConcurrencyMain {
         Thread myRunnableThread = new Thread(new MyRunnable(){
             @Override
             public void run() {
+                try{
+                    anotherThread.join();
+                    System.out.println("AnotherThread terminated so I'm running it again");
+                } catch (InterruptedException e) {
+                    System.out.println(ThreadColor.ANSI_CYAN + "I couldnt wait");
+                }
                 System.out.println(ThreadColor.ANSI_RED + "Hello from anonymous class's implementation myRunnable");
             }
         });
         myRunnableThread.start();
+        //anotherThread.interrupt();
 
         System.out.println(ThreadColor.ANSI_PURPLE + "Main thread again");
 
