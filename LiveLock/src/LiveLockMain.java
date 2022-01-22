ec82b500d730e01780ca0af3de7a881d6b3910aa
sertachak <sertaconurhakbilen@gmail.com>
2022-01-22 16:24:36 +0300

    Live lock

---
+++ LiveLock/src/LiveLockMain.java
@@ -0,0 +1,14 @@
+public class LiveLockMain {
+
+    public static void main(String[] args) {
+        final Worker worker1 = new Worker("Worker1", true);
+        final Worker worker2 = new Worker("Worker2", true);
+
+        final SharedResource sharedResource = new SharedResource(worker1);
+
+        new Thread(() -> worker1.work(sharedResource, worker2)).start();
+
+        new Thread(() -> worker2.work(sharedResource, worker1)).start();
+
+    }
+}
